76eef75ccadb3e4c04c3fa29f1c3f032e0ea1b9b
ghanizadev <jf.melo6@gmail.com>
2020-03-26 01:13:13 +0200

    timestamp added to all loggers

--- proxyServer/src/utils/logger.ts
+++ proxyServer/src/utils/logger.ts
@@ -1,26 +1,24 @@
 import winston from 'winston';
 
-const { combine, timestamp, label, printf } = winston.format;
+const { printf, json, timestamp } = winston.format;
 
 const consoleFormat = printf((data) => {
-	return `[${data.timestamp}] ${data.level.toUpperCase()}: ${data.message}`;
+	return `[${data.timestamp}]: ${data.level.toUpperCase()}: ${data.message}`;
 });
 
 const logger = winston.createLogger({
-	format: combine(
-		label({ label: 'proxy_server' }),
-        timestamp(),
-    ),
-	transports: [
-		new winston.transports.File({ filename: 'error.log', level: 'error' }),
-		new winston.transports.File({ filename: 'combined.log' }),
-	]
+    format: winston.format.combine(timestamp(), json()),
+    defaultMeta: { service: 'proxy_server' },
+    transports: [
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'events.log' })
+    ]
 });
 
 if (process.env.NODE_ENV !== 'production') {
 	logger.add(new winston.transports.Console({
-        format: consoleFormat
+        format: winston.format.combine(timestamp(), consoleFormat)
 	}));
-    }
+}
 
 export default logger;
